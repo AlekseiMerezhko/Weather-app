1daa5299b79fc8f229b081b8a7fa72dc655c6d97
Alex Merezhko <merezhko.aleksei@gmail.com>
2020-01-20 18:20:36 +0200

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
 import App from "./App";
-import { store } from "./store/configureStore";
+import { store, persistor } from "./store/configureStore";
 
 import "./css/index.css";
 import "./css/tailwind.css";
@@ -12,7 +13,9 @@ import "./css/tailwind.css";
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
   document.getElementById("root")
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,13 +2,28 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import rootReducer from "../reducers";
 import rootSaga from "../sagas";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["user", "cities"] // which reducer want to store
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
 const sagaMiddleware = createSagaMiddleware();
 
-export const store = createStore(
-  rootReducer,
+const store = createStore(
+  pReducer,
   composeWithDevTools(applyMiddleware(sagaMiddleware, logger))
 );
 
 sagaMiddleware.run(rootSaga);
+const persistor = persistStore(store);
+
+export { persistor, store };
