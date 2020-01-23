787e514aa8f3e48f0dba8c88e25941b4a7a86f01
Alex Merezhko <merezhko.aleksei@gmail.com>
2020-01-23 13:23:49 +0200

    Change footer text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,34 +6,34 @@ const Footer = () => {
       <div className="w-full md:flex justify-center text-white pl-4 pr-4 pb-2">
         <div className="md:w-1/3 flex justify-center">
           <div>
-            <h2 className="text-lg">FOOTER CONTENT</h2>
+            <h2 className="text-lg">Application info</h2>
             <p>
-              Here you can use rows and columns to organize your footer content.
+              This is test app with react, redux, formik, yup and accuweather
+              api
             </p>
           </div>
         </div>
         <div className="md:w-1/3 flex justify-center">
           <div>
-            <h2 className="text-lg">LINKS</h2>
+            <h2 className="text-lg">SOCIAL</h2>
             <ul>
-              <li>Link 1</li>
-              <li>Link 2</li>
-              <li>Link 3</li>
-              <li>Link 4</li>
-              <li>Link 5</li>
+              <li className="hover:text-black cursor-pointer">Facebook</li>
+              <li className="hover:text-black cursor-pointer">Linkedin</li>
+              <li className="hover:text-black cursor-pointer">Twitter</li>
+              <li className="hover:text-black cursor-pointer">Telegram</li>
+              <li className="hover:text-black cursor-pointer">YouTube</li>
             </ul>
           </div>
         </div>
         <div className="md:w-1/3 flex justify-center">
           {" "}
           <div>
-            <h2 className="text-lg">LINKS</h2>
+            <h2 className="text-lg">WORK</h2>
             <ul>
-              <li>Link 1</li>
-              <li>Link 2</li>
-              <li>Link 3</li>
-              <li>Link 4</li>
-              <li>Link 5</li>
+              <li className="hover:text-black cursor-pointer">Trello</li>
+              <li className="hover:text-black cursor-pointer">Slack</li>
+              <li className="hover:text-black cursor-pointer">GitHub</li>
+              <li className="hover:text-black cursor-pointer">Bitbucket</li>
             </ul>
           </div>
         </div>
